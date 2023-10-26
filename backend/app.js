40033565753ce2a47ef8f8dd85ef93e51fc8efd5
jzurc7 <100167348+jzurc7@users.noreply.github.com>
2023-10-25 20:12:54 -0500

    Revert "Complete Backend Skeleton"

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const cors = require('cors');
 const { db } = require('./db/db');
-const {readdirSync} = require('fs')
 const app = express()
 
 require('dotenv').config()
@@ -11,9 +10,6 @@ const PORT = process.env.PORT
 // middleware
 app.use(express.json())
 app.use(cors())
-
-// routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 const server = () => {
     app.listen(PORT, () => {
