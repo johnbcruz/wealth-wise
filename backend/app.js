416cd5ef6cbbb03ba2eb93663cf7a14b4b48e610
Richer Duong <duongricher@gmail.com>
2023-10-25 19:19:15 -0500

    Complete Backend Skeleton

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const { db } = require('./db/db');
+const {readdirSync} = require('fs')
 const app = express()
 
 require('dotenv').config()
@@ -10,6 +11,9 @@ const PORT = process.env.PORT
 // middleware
 app.use(express.json())
 app.use(cors())
+
+// routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
 const server = () => {
     app.listen(PORT, () => {
