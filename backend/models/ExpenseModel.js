79395dd85d9532ef3922c17e9d2d7e4bdd2a0850
Richer Duong <duongricher@gmail.com>
2023-11-10 16:27:34 -0600

    Update backend

--- backend/models/ExpenseModel.js
+++ backend/models/ExpenseModel.js
@@ -15,7 +15,7 @@ const ExpenseSchema = new mongoose.Schema({
     },
     type: {
         type: String,
-        default: "income"
+        default: "expense"
     },
     date: {
         type: Date,
