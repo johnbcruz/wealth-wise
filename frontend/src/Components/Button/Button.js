f16d9b30e0fa7af6b303666a64e0f6062b0cc0c6
Audrey Dang <audreydang4103@gmail.com>
2023-11-17 15:03:03 -0600

    Update Button.js

--- frontend/src/Components/Button/Button.js
+++ frontend/src/Components/Button/Button.js
@@ -3,24 +3,43 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function Button({name, icon, onClick, bg, bPad, color, bRad}) {
+function Button({ name, icon, onClick, bg = '#0077b6', bPad = '10px 20px', color = 'white', bRad = '5px' }) {
     return (
-        <ButtonStyled style={{
-            background: bg,
-            padding: bPad,
-            borderRadius: bRad,
-            color: color,
-        }} onClick={onClick}>
-            {icon}
+        <ButtonStyled
+            style={{
+                background: bg,
+                padding: bPad,
+                borderRadius: bRad,
+                color: color,
+            }}
+            onClick={onClick}
+        >
+            {icon && <IconContainer>{icon}</IconContainer>}
             {name}
         </ButtonStyled>
-    )
+    );
 }
 
 const ButtonStyled = styled.button`
     display: flex;
     align-items: center;
     cursor: pointer;
+    border: none;
+    outline: none;
+    transition: background-color 0.3s ease, box-shadow 0.3s ease;
+    font-weight: bold;
+    font-size: 1rem;
+
+    &:hover {
+        background-color: #0077b6; 
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+`;
+
+const IconContainer = styled.span`
+    display: flex;
+    align-items: center;
+    margin-right: 10px;
 `;
 
 export default Button
