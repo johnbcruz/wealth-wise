93bdd654cc7c74e0a6a99ddf73d2313f88c168ed
Richer Duong <duongricher@gmail.com>
2023-11-27 14:26:35 -0600

    Connect frontend to backend

--- frontend/src/Components/Items/Items.js
+++ frontend/src/Components/Items/Items.js
@@ -95,7 +95,7 @@ function IncomeItem({
 }
 
 const IncomeItemStyled = styled.div`
-    background: #FCF6F9;
+    // background: #FFFFFF;
     border: 2px solid #FFFFFF;
     box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
     border-radius: 20px;
